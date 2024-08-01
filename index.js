fbe34f9671406ac5b1933605dac358ab1aac8b00
Tundoor <thandwem045@gmail.com>
2024-08-01 16:26:45 +0200

    Created Boolean data Type

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-// 1. Create two variables, firstCard and secondCard. 
-// Set their values to a random number between 2-11
-
-// 2. Create a variable, sum, and set it to the sum of the two cards
+// Variebles 
 
 let firstCard = 15
 let secondCard = 9
-
 let sum = firstCard + secondCard
+let hasBlackJack = false
 
 //  Create If - else conditions
 
@@ -15,8 +12,11 @@ if (sum < 21) {
     console.log("Do you want to draw a new card? 🧐")
 }  else if (sum === 21 ) {
     console.log("Woohoo! You've got BlackJack!! 🤩")
+    hasBlackJack = true
 }  else if (sum > 21) {
      console.log("You Lose 😭")
 }
 
+
+// Cash Out 
 
