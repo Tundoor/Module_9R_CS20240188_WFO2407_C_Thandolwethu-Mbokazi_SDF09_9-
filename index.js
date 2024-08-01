5b9090f6e9b7838eb87e5b756bf2db3485128ed7
Tundoor <thandwem045@gmail.com>
2024-08-01 21:15:40 +0200

    Updated random number

--- index.js
+++ index.js
@@ -18,7 +18,14 @@ function startGame(){
 }
 
 function getRandomCard () {            //Added so a random card is rendered
-    return Math.floor(Math.random()*13 + 1 )
+    let randomNumber = Math.floor( Math.random()*13 ) + 1 
+       if (randomNumber > 10) {
+         return 10
+       } else if (randomNumber === 1){
+        return 11
+       } else {
+        return randomNumber
+       }
 }
 
 
